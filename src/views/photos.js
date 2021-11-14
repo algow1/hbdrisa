9032a6018685768a955b38f5c00fbb3434c2a208
Algo Wijaya <algow41@gmail.com>
2021-11-14 20:48:37 +0700

    add more photos

--- src/views/photos.js
+++ src/views/photos.js
@@ -13,9 +13,12 @@ import photo12 from '../photos/12.jpg';
 import photo13 from '../photos/13.jpg';
 import photo14 from '../photos/14.jpg';
 import photo15 from '../photos/15.jpg';
+import photo16 from '../photos/16.jpg';
+import photo17 from '../photos/17.jpg';
+import photo18 from '../photos/18.jpg';
 
 function getPhotos() {
-  const photoSource = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12, photo13, photo14, photo15];
+  const photoSource = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12, photo13, photo14, photo15, photo16, photo17, photo18];
   let photos = [];
   
   photoSource.forEach((value, key) => {
