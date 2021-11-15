63cd19b17a13c611cc95feb530f5e0329a74df0d
aalaksana <32954728+aalaksana@users.noreply.github.com>
2021-11-15 22:15:15 +0700

    tambah pesan algow1

--- src/views/Wisher.js
+++ src/views/Wisher.js
@@ -14,7 +14,7 @@ class Wisher extends Component {
       'Tercapai semua yang kamu impikan',
       'Semoga tahun depan lulus tubel',
       'Biar bisa sering pulang',
-      'Aku yakin kok sama kemampuanmu',
+      'Aku yakin sama kemampuanmu',
       'Yang paling penting....',
       'Semoga kamu selalu bahagia',
       'Selalu menjadi Risa yang apa adanya',
